975ee4d7f5654e96940b802e43f2fa6f56e4ec29
Sijin He <sijin@ebi.ac.uk>
2017-08-14 13:48:06 +0100

    update wiki url

--- src/app/shared/service/wiki/wiki.service.ts
+++ src/app/shared/service/wiki/wiki.service.ts
@@ -1,44 +1,57 @@
 import {Injectable} from '@angular/core';
 import {Http, Response, URLSearchParams} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import { AppConfig } from '../../../app.config';
 
 @Injectable()
 export class WikiService {
-  private baseUrl: string;
   private gitRepoName = 'phenomenal-h2020.wiki';
+  private baseUrl = '';
+  private metadataUrl: string;
+  private headUrl: string;
 
-  constructor(private http: Http) {
-    this.baseUrl = 'http://phenomenal-h2020.eu/wiki';
+  constructor(
+    private http: Http,
+    private config: AppConfig
+  ) {
+    // this.baseUrl = 'http://phenomenal-h2020.eu/wiki';
     // this.baseUrl  = 'http://localhost/FederatedGitWiki';
+    this.metadataUrl = '/php-phenomenal-portal-app-library';
+    if (config.getConfig('host') !== '') {
+      this.baseUrl = config.getConfig('host') + ':80';
+      this.headUrl = this.baseUrl + this.metadataUrl;
+    } else {
+      this.headUrl = this.metadataUrl;
+    }
   }
 
   loadMenu(): Observable<string[]> {
-    const url = this.baseUrl + '/wiki/controller.php?filename=' + this.gitRepoName + '&format=array';
+    const url = this.headUrl + '/wiki/controller.php?filename=' + this.gitRepoName + '&format=array';
     return this.http.get(url).map(this.extractData);
   }
 
   loadUserDocumentationMenu(): Observable<string[]> {
-    const url = this.baseUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=User-Documentation&format=array&limit=100';
+    const url = this.headUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=User-Documentation&format=array&limit=100';
     return this.http.get(url).map(this.extractData);
   }
 
   loadDeveloperDocumentationMenu(): Observable<string[]> {
-    const url = this.baseUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=Developer-Documentation&format=array&limit=100';
+    const url = this.headUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=Developer-Documentation&format=array&limit=100';
     return this.http.get(url).map(this.extractData);
   }
 
   loadTutorialsMenu(): Observable<string[]> {
-    const url = this.baseUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=Tutorials&format=array&limit=100';
+    const url = this.headUrl + '/wiki/menu.php?foldername=' + this.gitRepoName + '&filename=Tutorials&format=array&limit=100';
     return this.http.get(url).map(this.extractData);
   }
 
   loadPage(id) {
-    const url = this.baseUrl + '/wiki/page.php?foldername=' + this.gitRepoName + '&filename=' + id + '&format=array&limit=3';
+    const url = this.headUrl + '/wiki/page.php?foldername=' + this.gitRepoName + '&filename=' + id + '&format=array&limit=3';
     return this.http.get(url).map(this.extractData);
   }
 
   search(term: string) {
-    const url = this.baseUrl + '/wiki/search.php?term=' + term;
+    const url = this.headUrl + '/wiki/search.php?term=' + term;
 
     const params = new URLSearchParams();
     params.set('search', term); // the user's search value
