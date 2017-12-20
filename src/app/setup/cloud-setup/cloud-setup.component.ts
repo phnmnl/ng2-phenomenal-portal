83ccc9ea6a792fe26cdc5a3940e8eb617cd3d6d5
Marco Enrico Piras <kikkomep@crs4.it>
2017-12-20 20:08:54 +0100

    Subscribe the CloudSetupComponent to be notified of the current user

--- src/app/setup/cloud-setup/cloud-setup.component.ts
+++ src/app/setup/cloud-setup/cloud-setup.component.ts
@@ -1,20 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { CloudProvider } from '../cloud-provider';
+import { UserService } from "../../shared/service/user/user.service";
+import { User } from "../../shared/service/user/user";
+import { AppConfig } from "../../app.config";
 
 @Component({
   selector: 'ph-cloud-setup',
   templateUrl: './cloud-setup.component.html',
-  styleUrls: ['./cloud-setup.component.scss']
+  styleUrls: ['./cloud-setup.component.scss'],
+  providers: [UserService]
 })
 
 export class CloudSetupComponent implements OnInit {
 
   @Input() cloudProvider: CloudProvider;
 
+  private user: User;
+
+  constructor(public userService: UserService, private config: AppConfig) {
+    this.user = this.userService.getCurrentUser();
+    console.log("Current user: ", this.user);
   }
 
   ngOnInit() {
+    this.userService.currentUserObservable.subscribe(user => {
+      this.user = <User> user;
+      if(user) {
+        console.log("*** Has Galaxy account: " + this.user.hasGalaxyAccount);
+      }
+      console.log("Updated user @ CloudSetupComponent", user, this.user)
+    });
+    console.log("Cloud provider user", this.user);
   }
+
 
   get galaxy_instance_url(): string {
     console.debug("Galaxy instance URL:", this.config.getConfig("galaxy_url"));
