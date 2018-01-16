55100499b3d8da89c05f40d786c072b613896ccd
Marco Enrico Piras <kikkomep@crs4.it>
2018-01-16 18:35:10 +0100

    Update guards and login component to support a proper redirection after login

--- src/app/shared/guard/AcceptedTermsGuard.ts
+++ src/app/shared/guard/AcceptedTermsGuard.ts
@@ -1,65 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import {UserService} from "app/shared/service/user/user.service";
-import {AuthService} from "ng2-cloud-portal-service-lib";
-import {Observable} from "rxjs/Observable";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import { UserService } from "app/shared/service/user/user.service";
+import { AuthService } from "ng2-cloud-portal-service-lib";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class AcceptedTermsGuard implements CanActivate {
 
   constructor(private router: Router,
               private userService: UserService,
-              private authService: AuthService) { }
+              private authService: AuthService) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-    console.log("Route", route, route.url.pop());
-    console.log("Route", route);
-
-    console.log("State", state);
-
-    console.log("Current user", this.userService.getCurrentUser());
+    // detect route and parameters
+    let returnUrl = state.url;
+    let queryParams = returnUrl ? {'returnUrl': returnUrl} : {};
 
     if (this.authService.credentialService.getUsername()) {
       return this.userService.findById(this.authService.credentialService.getUsername()).map(
-          (userInfo) => {
-            console.log("User info from backend service", userInfo);
-            if (userInfo && "Idmetadata" in userInfo) {
-              console.log("Fetched user info: ", userInfo);
-              console.log("Route allowed");
-              return true;
-            } else {
-              console.log("User with id '" + this.authService.credentialService.getUsername() + "' not found!");
-              // this.logout();
-              // this.userService.logout();
-              this.router.navigate(['/login']);
-              return false;
-            }
-          },
-          (err) => {
-            console.error(err);
+        (userInfo) => {
+          console.log("User info from backend service", userInfo);
+          if (userInfo && "Idmetadata" in userInfo) {
+            console.log("Fetched user info: ", userInfo);
+            console.log("Route allowed");
+            return true;
+          } else {
+            console.log("User with id '" + this.authService.credentialService.getUsername() + "' not found!");
             this.userService.logout();
-            this.router.navigate(['/login']);
+            this.router.navigate(['/login'], {queryParams: queryParams});
             return false;
           }
+        },
+        (err) => {
+          console.error(err);
+          this.userService.logout();
+          this.router.navigate(['/login'], {queryParams: queryParams});
+          return false;
+        }
       ).first();
-    }else {
+    } else {
       console.log("Ops.....");
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], {queryParams: queryParams});
       return false;
     }
-
-
-    
-
-
-    // return true;
-    // if (localStorage.getItem('currentUser')) {
-    //   // logged in so return true
-    //   return true;
-    // }
-    //
-    // // not logged in so redirect to login page with the return url
-    // this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-    // return false;
   }
 }
--- src/app/shared/guard/UserAuthenticatedGuard.ts
+++ src/app/shared/guard/UserAuthenticatedGuard.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import {UserService} from "app/shared/service/user/user.service";
-import {AuthService} from "ng2-cloud-portal-service-lib";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import { UserService } from "app/shared/service/user/user.service";
+import { AuthService } from "ng2-cloud-portal-service-lib";
 
 @Injectable()
 export class UserAuthenticatedGuard implements CanActivate {
 
   constructor(private router: Router,
               private userService: UserService,
-              private authService: AuthService) { }
+              private authService: AuthService) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     console.log("Checking if there exists a user in the current local session");
     if (this.authService.credentialService.getUsername()) {
       console.log("A user exists in the current local session");
       return true;
-    }else {
+    } else {
       console.log("A user doesn't exist in the current local session");
       // force cleaning local session
       this.userService.logout();
+      // get return url from route parameters or default to '/'
+      let returnUrl = state.url;
+      let queryParams = returnUrl ? {'returnUrl': returnUrl} : {};
       // avoid the requested route and redirect to login
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], {queryParams: queryParams});
       return false;
     }
   }
