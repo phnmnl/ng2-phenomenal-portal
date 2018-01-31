f7de67f0872532d4ca24ce3951096e8840438c50
Marco Enrico Piras <kikkomep@crs4.it>
2018-01-31 19:45:33 +0100

    Add periodic check of user authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
 import { BreadcrumbService } from './shared/component/breadcrumb/breadcrumb.service';
+import { UserService } from "./shared/service/user/user.service";
+import { ActivatedRoute, NavigationStart, Router } from "@angular/router";
 
 @Component({
   selector: 'ph-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  private returnUrl;
+  private previousUrl;
+  private checkAuthorization;
 
   constructor(private breadcrumbService: BreadcrumbService,
+              public userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router,
               private viewContainerRef: ViewContainerRef) {
+    // register route names
     breadcrumbService.addFriendlyNameForRoute('/', 'Home');
     breadcrumbService.addFriendlyNameForRoute('/home', 'Home');
     breadcrumbService.addFriendlyNameForRoute('/app-library', 'App Library - Service Catalogue');
@@ -31,8 +41,56 @@ export class AppComponent {
     breadcrumbService.addFriendlyNameForRoute('/cre-dashboard', 'Cloud Research Environment Dashboard');
     // hidden routes
     breadcrumbService.hideRoute('/logs');
-
     // You need this small hack in order to catch application root view container ref
     this.viewContainerRef = viewContainerRef;
+  }
+
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || null;
+    let user = this.userService.getCurrentUser();
+    if (user) {
+      this.registerAuthChecker();
+    } else {
+      this.userService.getObservableCurrentUser().subscribe(user => {
+        this.registerAuthChecker();
+      });
+    }
+    this.router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe((event: NavigationStart) => {
+          this.previousUrl = event.url;
+      });
+  }
+
+  ngOnDestroy() {
+    this.deRegisterAuthChecker();
+  }
+
+  private registerAuthChecker() {
+    this.checkAuthorization = setInterval(() => {
+      console.log("Periodic authorization check...");
+      this.userService.isUserAuthorized().subscribe(
+        (user) => {
+          if (!user) this.redirectToLogin();
+        },
+        () => {
+          console.log("User not authenticated!!!");
+          this.redirectToLogin();
+        });
+    }, 1000 * 60);
+  }
+
+  private deRegisterAuthChecker() {
+    if (this.checkAuthorization) {
+      console.log("Cancelling periodic authorization check...");
+      clearInterval(this.checkAuthorization);
+    }
+  }
+
+  private redirectToLogin() {
+    let returnUrl = this.route.snapshot.queryParams['returnUrl'] || this.previousUrl;
+    let queryParams = returnUrl ? {'returnUrl': returnUrl } : {};
+    this.deRegisterAuthChecker();
+    this.router.navigate(['/login'], {queryParams: queryParams});
   }
 }
