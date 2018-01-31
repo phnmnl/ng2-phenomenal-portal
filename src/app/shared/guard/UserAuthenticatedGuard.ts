8bf28096d617e116e3a7538b3095e5389704f120
Marco Enrico Piras <kikkomep@crs4.it>
2018-01-31 17:04:15 +0100

    Update and apply route guards

--- src/app/shared/guard/UserAuthenticatedGuard.ts
+++ src/app/shared/guard/UserAuthenticatedGuard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { UserService } from "app/shared/service/user/user.service";
 import { AuthService } from "ng2-cloud-portal-service-lib";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class UserAuthenticatedGuard implements CanActivate {
@@ -13,19 +14,21 @@ export class UserAuthenticatedGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     console.log("Checking if there exists a user in the current local session");
-    if (this.authService.credentialService.getUsername()) {
-      console.log("A user exists in the current local session");
-      return true;
-    } else {
-      console.log("A user doesn't exist in the current local session");
-      // force cleaning local session
-      this.userService.logout();
-      // get return url from route parameters or default to '/'
-      let returnUrl = state.url;
-      let queryParams = returnUrl ? {'returnUrl': returnUrl} : {};
-      // avoid the requested route and redirect to login
-      this.router.navigate(['/login'], {queryParams: queryParams});
-      return false;
-    }
+    return this.userService.isUserAuthorized()
+      .map((user) => {
+        console.log("User properly authenticated", user);
+        return true;
+      })
+      .catch((error) => {
+        console.log("A user doesn't exist in the current local session");
+        // force cleaning local session
+        this.userService.logout();
+        // get return url from route parameters or default to '/'
+        let returnUrl = state.url;
+        let queryParams = returnUrl ? {'returnUrl': returnUrl} : {};
+        // avoid the requested route and redirect to login
+        this.router.navigate(['/login'], {queryParams: queryParams});
+        return Observable.throw(false);
+      });
   }
 }
