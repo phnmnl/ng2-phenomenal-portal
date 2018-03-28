9fcba0966d74161bf222276e41059432c939b5c8
Marco Enrico Piras <kikkomep@crs4.it>
2018-03-28 17:03:20 +0200

    Fix AppConfig injection the FooterComponent test

--- src/app/shared/component/footer/footer.component.spec.ts
+++ src/app/shared/component/footer/footer.component.spec.ts
@@ -1,10 +1,17 @@
 /* tslint:disable:no-unused-variable */
 
-import {FooterComponent} from './footer.component';
+import { FooterComponent } from './footer.component';
+import { AppConfig } from "../../../app.config";
+import { inject } from "@angular/core/testing";
 
 describe('Component: Footer', () => {
+  let appConfig: AppConfig;
+  beforeEach(inject([AppConfig], (config: AppConfig) => {
+    appConfig = config;
+  }));
+  
   it('should create an instance', () => {
-    const component = new FooterComponent();
+    const component = new FooterComponent(appConfig);
     expect(component).toBeTruthy();
   });
 });
