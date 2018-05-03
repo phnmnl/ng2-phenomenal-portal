b9ee3561aeccbfe5b3a0722090a406fa81eb3d41
Marco Enrico Piras <kikkomep@crs4.it>
2018-05-03 16:25:02 +0200

    Move AppLibrary creation into the AppLibraryService

---
+++ src/app/shared/service/application-library/categories.ts
@@ -0,0 +1,11 @@
+export class AppLibraryCategories {
+  functionality = [];
+  approaches = [];
+  instrument = [];
+
+  constructor(functionality: any[], approaches: any[], instrument: any[]) {
+    this.functionality = functionality;
+    this.approaches = approaches;
+    this.instrument = instrument;
+  }
+}
