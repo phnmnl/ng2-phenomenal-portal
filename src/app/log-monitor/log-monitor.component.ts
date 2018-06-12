a68ebeda06a1f512570af8319fe24fb45280580a
Marco Enrico Piras <kikkomep@crs4.it>
2018-06-12 12:34:24 +0200

    Combine Deploy and Destroy logs

--- src/app/log-monitor/log-monitor.component.ts
+++ src/app/log-monitor/log-monitor.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { DeploymentInstance } from "../shared/service/deployer/deploymentInstance";
-import { DeployerService } from "../shared/service/deployer/deployer.service";
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { DeployementService } from "../shared/service/deployer/deployement.service";
 import { ActivatedRoute } from "@angular/router";
-import { Deployment } from "ng2-cloud-portal-service-lib";
+
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Deployment } from "../shared/service/deployer/deployment";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'ph-log-monitor',
@@ -12,13 +13,19 @@ import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 })
 export class LogMonitorComponent implements OnInit {
 
+  private deployLogs: string;
+  private destroyLogs: string;
+
+  logs: string;
   autoScrollDown: boolean = false;
+  @Output() logUpdatedEventEmitter = new EventEmitter();
+
   public deployment: Deployment;
   public downloadLogsUri: SafeUrl;
 
   constructor(private route: ActivatedRoute,
               private sanitizer: DomSanitizer,
-              private deployerManager: DeployerService) {
+              private deployerManager: DeployementService) {
   }
 
   public toggleScrollDown() {
@@ -32,23 +39,28 @@ export class LogMonitorComponent implements OnInit {
   }
 
   ngOnInit() {
+    let combinedLogs: string = "";
     let reference = this.route.snapshot.queryParams['id'];
     this.deployerManager.getDeployment(reference).subscribe(
       (data) => {
         this.deployment = data;
-        this.deployerManager.getDeploymentLogs(reference).subscribe(
-          (logs) => {
-            this.deployment["logs"] = logs;
-            let blob = new Blob([this.deployment["logs"]], {type: 'text/plain'});
-            let url = window.URL.createObjectURL(blob);
-            this.downloadLogsUri = this.sanitizer.bypassSecurityTrustUrl(url);
-          }, (error) => {
-            console.error(error);
+        this.setDeployLogs(this.deployment.deployLogs);
+        this.updateLogs(this.deployLogs);
+        if (this.deployment.isStarting() || this.deployment.isStartedFailed() || this.deployment.isRunning()) {
+          this.deployment.getDeployLogsAsObservable().subscribe((logs) => {
+            this.setDeployLogs(this.deployment.deployLogs);
+            this.updateLogs(this.deployLogs);
+            console.log("Updating logs on start");
           });
-        if (this.deployment["status"] === "STARTING") {
-          this.deployerManager.monitorDeploymentLogs(this.deployment, 1000, () => {
-            if (this.autoScrollDown)
-              this.scrollDown();
+        }
+
+        if (this.deployment.isDestroying() || this.deployment.isDestroyed()) {
+          this.setDestroyLogs(this.deployment.destroyLogs);
+          this.updateLogs(this.deployLogs + this.destroyLogs);
+          this.deployment.getDestroyLogsAsObservable().subscribe((logs) => {
+            this.setDestroyLogs(logs);
+            this.updateLogs(this.deployLogs + this.destroyLogs);
+            console.log("Updating logs on remove");
           });
         }
       },
@@ -60,5 +72,35 @@ export class LogMonitorComponent implements OnInit {
   ngOnDestroy() {
   }
 
+  private prepareForDownload(logs) {
+    let blob = new Blob([logs], {type: 'text/plain'});
+    let url = window.URL.createObjectURL(blob);
+    this.downloadLogsUri = this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 
+  private updateLogs(logs) {
+    this.logs = logs;
+    this.prepareForDownload(logs);
+    if (this.autoScrollDown)
+      this.scrollDown();
+    this.logUpdatedEventEmitter.emit(this.logs);
+  }
+
+  private cleanLogs() {
+    this.logs = "";
+  }
+
+  private static HR = "*".repeat(150);
+
+  private setDeployLogs(logs) {
+    this.deployLogs =
+      LogMonitorComponent.HR + "\n*** DEPLOY LOGS ***\n"
+      + LogMonitorComponent.HR + "\n\n" + logs;
+  }
+
+  private setDestroyLogs(logs) {
+    this.destroyLogs = "\n\n\n\n"
+      + LogMonitorComponent.HR + "\n*** DESTROY LOGS ***\n"
+      + LogMonitorComponent.HR + "\n\n" + logs;
+  }
 }
