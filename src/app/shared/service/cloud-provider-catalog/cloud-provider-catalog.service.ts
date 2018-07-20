b6121231ca92b5a07232e2203f8393623575c15e
Marco Enrico Piras <kikkomep@crs4.it>
2018-07-20 11:33:09 +0200

    Integrate not preconfigured providers within the CloudProvidersCatalogService

--- src/app/shared/service/cloud-provider-catalog/cloud-provider-catalog.service.ts
+++ src/app/shared/service/cloud-provider-catalog/cloud-provider-catalog.service.ts
@@ -4,6 +4,7 @@ import { TokenService } from "ng2-cloud-portal-service-lib";
 import { AppConfig } from "../../../app.config";
 import { Observable } from "rxjs/Observable";
 import { CloudProvider } from "../deployer/cloud-provider";
+import { ProviderRegistry } from "./provider-registry";
 
 @Injectable()
 export class CloudProviderCatalogService {
@@ -15,32 +16,26 @@ export class CloudProviderCatalogService {
               private config: AppConfig) {
   }
 
+  public getPhenomenalProvider() {
+    return ProviderRegistry.getPhenomenalProvider();
+  }
+
   public getProviders() {
+    return ProviderRegistry.getProviders();
+  }
+
+  public getPreconfiguredProviders() {
     return this.http.get(this.URL)
       .map((res) => {
         let providers = [];
         let data = res.json()['data'];
-        for(let p of data){
+        for (let p of data) {
           p["preset"] = p["id"];
           p["name"] = "ostack";
           p["preconfigured"] = true;
           providers.push(new CloudProvider(p));
         }
         return providers;
-      }).catch(res => Observable.throw(res.json()));
-  }
-
-  public getProvider(providerName: string) {
-    return this.http.get(this.URL + "/" + providerName)
-      .map((res) => {
-        return res.json()['data'];
-      }).catch(res => Observable.throw(res.json()));
-  }
-
-  public getProviderCredentials(providerName: string) {
-    return this.http.get(this.URL + "/" + providerName + "/credentials")
-      .map((res) => {
-        return res.json()['data'];
       }).catch(res => Observable.throw(res.json()));
   }
 
