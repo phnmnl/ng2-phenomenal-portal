9b63d78be77b7e855a8f57a4f9b9a443d33b489a
Marco Enrico Piras <kikkomep@crs4.it>
2018-07-20 11:55:35 +0200

    Move the logic to get the provider logo to the component controller

--- src/app/cre-dashboard/cre-dashboard.component.ts
+++ src/app/cre-dashboard/cre-dashboard.component.ts
@@ -12,6 +12,7 @@ import { CanComponentDeactivate } from "../shared/guard/CanDeactivateGuard";
 import { DeployementService } from "../shared/service/deployer/deployement.service";
 import { ModalDialogContentComponent } from "../shared/component/modal-dialog/modal-dialog.component";
 import { Observable } from "rxjs";
+import { CloudProviderCatalogService } from "../shared/service/cloud-provider-catalog/cloud-provider-catalog.service";
 
 @Component({
   selector: 'ph-cre-dashboard',
@@ -32,9 +33,12 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
   public luigi_logo = 'assets/img/logo/luigi.png';
   public jupyter_logo = 'assets/img/logo/jupyter_square.png';
 
+  private preconfigured_provider_logos = [];
+
   public error_icon = 'assets/img/error-icon.png';
 
   deploymentServerList: Deployment[];
+  private _preconfiguredCloudProviderCollection: null;
 
   private onDestroyEvent = new EventEmitter<Deployment>();
   private onDeleteEvent = new EventEmitter<Deployment>();
@@ -43,6 +47,7 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
   constructor(private modalService: NgbModal,
               private tokenService: TokenService,
               public credentialService: CredentialService,
+              private catalogService: CloudProviderCatalogService,
               public userService: UserService,
               private router: Router,
               private deploymentManager: DeployementService) {
@@ -56,6 +61,14 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
     }, (error) => {
       console.error("The current error", error);
       this.userService.logout();
+    });
+
+    this.catalogService.getPreconfiguredProviders().subscribe((providers) => {
+      this._preconfiguredCloudProviderCollection = providers;
+      for (let p of providers) {
+        this.preconfigured_provider_logos[p.preset] = p.logo.path;
+      }
+      console.log("Preconfigured Provider logos", this.preconfigured_provider_logos, providers);
     });
 
     this.onDestroyEvent.subscribe((deployment) => {
@@ -74,12 +87,12 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
     console.log("Checking if it can be unloaded");
-    for(let d of this.deploymentServerList){
-      if(d.isStarting()){
+    for (let d of this.deploymentServerList) {
+      if (d.isStarting()) {
         let status = d.statusTransition;
-        if(status){
+        if (status) {
           console.log(status);
-          if(status.stepNumber < 9) {
+          if (status.stepNumber < 9) {
             console.warn("You cannot deactivate the current window");
             return false;
           }
@@ -95,6 +108,7 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
     this.credentialService.clearCredentials();
     this.router.navigateByUrl('/login');
   }
+
 
   enabledServices(deployment: Deployment) {
     return deployment && deployment["status"] === "RUNNING"
@@ -141,6 +155,22 @@ export class CreDashboardComponent extends CanComponentDeactivate implements OnI
   public processDeleteDeployment(deployment: Deployment) {
     console.log("Deleting deployment ...");
     this.deploymentManager.deleteDeploymentConfiguration(deployment);
+  }
+
+
+  public getProviderLogo(deployment: Deployment) {
+    if (!deployment.usePreset()) {
+      if (deployment.useAwsProvider())
+        return this.aws_logo;
+      if (deployment.useOStackProvider())
+        return this.openstack_logo;
+      if (deployment.useGcpProvider())
+        return this.gce_logo;
+    } else {
+      if (deployment.preset in this.preconfigured_provider_logos)
+        return this.preconfigured_provider_logos[deployment.preset];
+      return this.generic_cloud_logo;
+    }
   }
 
   private static clearErrors(deployment: Deployment) {
