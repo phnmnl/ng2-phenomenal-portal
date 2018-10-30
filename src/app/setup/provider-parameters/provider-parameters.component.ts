82fe957cfa131063ad7330042cb7490bf92f9e41
Luca Pireddu <pireddu@crs4.it>
2018-10-30 18:16:40 +0100

    Disable Cerebellin
    It's not currently deployable by cloud-deploy-kubenow

--- src/app/setup/provider-parameters/provider-parameters.component.ts
+++ src/app/setup/provider-parameters/provider-parameters.component.ts
@@ -73,11 +73,12 @@ export class ProviderParametersComponent implements OnInit, OnChanges {
               private appConfig: AppConfig,
               private cloudProviderMetadataService: CloudProviderMetadataService) {
     this.phenVersions = [
-     {
-        id: "2.0-Cerebellin-20180523",
-        name: 'v2018.02 Cerebellin',
-        url: "https://github.com/phnmnl/cloud-deploy-kubenow-cerebellin-20180523"
-      },
+      /* Disable this version as it currently won't deploy due to a problem with cloud-deploy-kubenow
+      {
+         id: "2.0-Cerebellin-20180523",
+         name: 'v2018.02 Cerebellin',
+         url: "https://github.com/phnmnl/cloud-deploy-kubenow-cerebellin-20180523"
+      },*/
       {
         id: "3.0-Dalcotidine-20180801",
         name: 'v2018.08 Dalcotidine',
